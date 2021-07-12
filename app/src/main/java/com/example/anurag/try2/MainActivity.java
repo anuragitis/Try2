111b3472fbefb98db3a1d59ede7f79f94933fd21
Anurag Yadav <anurag.yadav@blackbuck.com>
2021-07-12 20:09:09 +0530

    my new feature

--- app/src/main/java/com/example/anurag/try2/MainActivity.java
+++ app/src/main/java/com/example/anurag/try2/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        // my1
+        // my2
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
